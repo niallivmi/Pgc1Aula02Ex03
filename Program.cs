33a82db2250ac3332102e70aec78081757e9dac8
Maria Julia Cavalcanti <majucavalcantix@gmail.com>
2024-03-22 20:39:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//@niallivmi
+using System;
+
+namespace Pgc1Aula02Ex03 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            float km, lt;
+
+            Console.Write("Digite a quantidade de quilômetros percorridos: ");
+            km = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de combustível consumido: ");
+            lt = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("O consumo médio do veículo é de: "+(km/lt)+" litros por 1km");
+            Console.ReadKey();
+        }
+    }
+}
